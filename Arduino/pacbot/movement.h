71c5c704f24c3d5b48a72ec19319b2d10fd1c95a
eay4074 <eay4074@rit.edu>
2018-02-18 16:32:05 -0500

    Added climb and strafe functions

--- Arduino/pacbot/movement.h
+++ Arduino/pacbot/movement.h
@@ -2,11 +2,15 @@
 
 
 
-void strafe(int pwm){
-  
+void strafe(int pwmValue){
+  Motor1(pwmValue);
+  Motor2(pwmValue);
+  Motor3((int)(-0.5*pwmValue));
 }
 
 
-void climb(int pwm){
-
+void climb(int pwmValue){
+  Motor1(pwmValue);
+  Motor2(-pwmValue);
+  Motor3(0)
 }
