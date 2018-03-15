4f26bf9f0b20a4a2bf359c0555df63f40d741616
eay4074 <eay4074@rit.edu>
2018-03-15 16:55:55 -0400

    changed AI from Genetic to standard robotic

---
+++ PacbotModule.py
@@ -0,0 +1,5 @@
+"""
+PacbotModule.py
+holds all functionality relevant to the game rule's
+Ethan Yaccarino-Mims
+"""
---
+++ constants.py
@@ -0,0 +1,8 @@
+"""
+constants.py
+holds all constants relevant to the robot's AI
+Ethan Yaccarino-Mims
+"""
+
+
+
