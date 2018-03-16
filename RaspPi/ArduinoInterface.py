ecca53420d65cd1aa3a9298b270768f24e9a86e4
eay4074 <eay4074@rit.edu>
2018-03-16 16:27:29 -0400

    started serial connection

--- RaspPi/ArduinoInterface.py
+++ RaspPi/ArduinoInterface.py
@@ -3,3 +3,12 @@ ArduinoInterface.py
 contains functionality for communication between RaspberryPi and Arduino
 Ethan Yaccarino-Mims
 """
+
+
+import serial
+import RPi.GPIO as GPIO
+
+
+def ArduinoInit():
+    ser = serial.Serial("/dev/ttyACM&", 9600) #replace & with num found from ls /dev/tty/ACM*
+    ser.baudrate = 9600
