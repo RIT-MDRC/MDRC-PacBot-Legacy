823e1c0d40488a3e81a36183f98e1e2961dbfe1b
eay4074 <eay4074@rit.edu>
2018-03-31 09:55:32 -0400

    changed from omni wheels to tank drive

--- RaspPi/Arduino/pacbot/motors.h
+++ RaspPi/Arduino/pacbot/motors.h
@@ -5,9 +5,8 @@
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
-Adafruit_DCMotor *myMotor1 = AFMS.getMotor(1);
-Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
-Adafruit_DCMotor *myMotor3 = AFMS.getMotor(3);
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
 
 
 void InitMotors(void)
@@ -18,23 +17,18 @@ void InitMotors(void)
   pinMode(InA_2, OUTPUT);
   pinMode(InB_2, OUTPUT);
   pinMode(PWM_2, OUTPUT);
-  pinMode(InA_3, OUTPUT);
-  pinMode(InB_3, OUTPUT);
-  pinMode(PWM_3, OUTPUT);
 
   AFMS.begin();
 
-  myMotor1->setSpeed(0);
-  myMotor2->setSpeed(0);
-  myMotor3->setSpeed(0);
+  leftMotor->setSpeed(0);
+  rightMotor->setSpeed(0);
 
-  myMotor1->run(FORWARD);
-  myMotor2->run(FORWARD);
-  myMotor3->run(FORWARD);
+  leftMotor->run(FORWARD);
+  rightMotor->run(FORWARD);
 }
 
 
-int drive1(int pwmValue)
+int driveLeft(int pwmValue)
 {
   if(pwmValue >= 0)
   {
@@ -49,7 +43,7 @@ int drive1(int pwmValue)
 }
 
 
-int drive2(int pwmValue)
+int driveRight(int pwmValue)
 {
   if(pwmValue >= 0)
   {
@@ -64,26 +58,6 @@ int drive2(int pwmValue)
 }
 
 
-int drive3(int pwmValue)
-{
-  if(pwmValue >= 0)
-  {
-    myMotor3 -> run(FORWARD); 
-  }
-  else
-  {
-    myMotor3 -> run(BACKWARD);
-  }
-  myMotor3->setSpeed(pwmValue);
-  
-}
-
-
-void driveAll(int dir[])
-{
-  
-  
-}
 
 
 
