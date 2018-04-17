abdeefb0a496fb8f0b9c8d5c7606208140a6c014
eay4074 <eay4074@rit.edu>
2018-04-17 17:45:43 -0400

    added code to test motors

---
+++ motor tests/motorTests/motorTests.ino
@@ -0,0 +1,52 @@
+
+
+#include "Pinout.h"
+#include "motors.h"
+
+
+int motorSpeed = 0;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  delay(1000);  
+
+  //starts the indication LED
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  
+  InitMotors();
+
+  delay(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  char msg[80];
+
+  //the char from the serial port
+  char cmd;
+
+  //toggle LED
+  digitalWrite(LED_PIN, HIGH);
+
+  if(Serial.available())
+  {
+    cmd = Serial.read();
+    switch(cmd)
+    {
+      case 'u':
+        motorSpeed += 10;
+        break;
+      case 'd':
+        motorSpeed -= 10;
+        break;
+    }
+  }
+  driveLeft(motorSpeed);
+  driveRight(motorSpeed);
+    
+} 
---
+++ motor tests/motorTests/motors.h
@@ -0,0 +1,85 @@
+/**
+ * motors.h
+ * Ethan Yaccarino-Mims
+ * adds driving functionality
+ */
+
+#include "Pinout.h"
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+//the Motor Shield object
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+//the motor objects
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
+
+
+/**
+ * initializes all objects and values
+ */
+void InitMotors(void)
+{
+  //defines pin modes
+  pinMode(InA_1, OUTPUT);
+  pinMode(InB_1, OUTPUT);
+  pinMode(PWM_1, OUTPUT);
+  pinMode(InA_2, OUTPUT);
+  pinMode(InB_2, OUTPUT);
+  pinMode(PWM_2, OUTPUT);
+
+  //starts the motor shield
+  AFMS.begin();
+
+  //initial values for motor speed
+  leftMotor -> setSpeed(0);
+  rightMotor -> setSpeed(0);
+
+  //initial direction for motors
+  leftMotor -> run(FORWARD);
+  rightMotor -> run(FORWARD);
+}
+
+
+/**
+ * drives the left motor at the given speed
+ * 
+ * @param pwmValue the speed to drive the motor at 
+ * on the scale of a signed eight bit value
+ */
+int driveLeft(int pwmValue)
+{
+  if(pwmValue >= 0)
+  {
+    leftMotor -> run(FORWARD); 
+  }
+  else
+  {
+    leftMotor -> run(BACKWARD);
+  }
+  leftMotor -> setSpeed(pwmValue);
+  
+}
+
+
+/**
+ * drives the right moter at the given speed
+ *
+ * @param pwmValue the speed to drive the motor at 
+ * on the scale of a signed eight bit value
+ */ 
+int driveRight(int pwmValue)
+{
+  if(pwmValue >= 0)
+  {
+    rightMotor -> run(FORWARD); 
+  }
+  else
+  {
+    rightMotor -> run(BACKWARD);
+  }
+  rightMotor -> setSpeed(pwmValue);
+  
+}
