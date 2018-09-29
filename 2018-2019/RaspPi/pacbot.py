0801058cb620ef51999c32b5e4558167cad5c2b1
Andrew Serra <acs8929@rit.edu>
2018-09-29 17:02:41 -0400

    fixed duplicate functions

--- 2018-2019/RaspPi/pacbot.py
+++ 2018-2019/RaspPi/pacbot.py
@@ -5,26 +5,6 @@ from helper import *
 checkpoint_interval = 5 # The interval of being reported per genome in generation
 num_generations = 300   # Number of generations
 
-# Activation: Softmax function
-def softmax(vec):
-    # Convert to numpy array
-    output_layer_in = np.array(vec)
-    # Set numerator values from output_layer_in
-    exponents = [np.exp(row) for row in output_layer_in]
-    # Set denominator
-    sum_exp = sum(exponents)
-    # Create the output layer activation values
-    output_vec = [exp/sum_exp for exp in exponents]
-
-    assert output_layer_in.shape == (4,1)
-
-    return output_vec
-
-# Calculate the percentage error
-def calc_error(inp, expected):
-    percentage_error = abs(inp - expected) / expected * 100
-    return percentage_error
-
 # Called to evaluate all genomes
 def eval_genomes(genomes, config):
     pass
