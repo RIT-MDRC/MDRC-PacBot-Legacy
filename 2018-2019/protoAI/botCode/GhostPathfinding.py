8cbf4f97d9cab2199f785182627bcb93c9840e03
eay4074 <eay4074@rit.edu>
2018-11-10 15:11:13 -0500

    Started with pathfinding

---
+++ 2018-2019/protoAI/botCode/GhostPathfinding.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+def getDirection(grid, ghost, pacman):
+    path = getPath(grid, ghost, pacman)
+    return directionOfPath(path), lengthOfPath(path) 
+
+
+
+def getPath(grid, ghost, pacman):
+    return 0
+
+
+
+def lengthOfPath(path):
+    return 0
+
+
+def directionOfPath(path):
+    return 0
