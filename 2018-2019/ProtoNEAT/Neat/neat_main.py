05ab8f0888f9149e1eefa4a8ecfa068aa4301c85
Prangon Das <pcd3897@g.rit.edu>
2018-12-03 20:31:52 -0500

    Introduced __init__.py files for importing and commented out neat_main

--- 2018-2019/ProtoNEAT/Neat/neat_main.py
+++ 2018-2019/ProtoNEAT/Neat/neat_main.py
@@ -1,11 +1,29 @@
-import neat
+
+""" Controls NEAT algorithm using the eval_genomes and the run method."""
+
 import os
+import neat
+# Import neat library
+# Find a way to import ghostpathfinding in protoAI botCode
 
+""" Evaluates the genomes until they match the Simple AI requirements in terms of movement direction """
 def eval_genomes(genomes, config):
-    pass
+    for genome_id, genome in genomes:
+        # Network gets output for which direction to go to.
+        # Assessing the model based on how much points it got
+        # Select the ones that are doing better in points
+        # not in movements
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        netOut = net.activate()
 
-def run(config):
-    pass
+
+
+def run(config_file):
+    # Load configuration.
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_file)
+
 
 
 if __name__ == "__main__":
