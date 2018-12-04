5cbf982de1cfd6bc567ffc314f3d2cb205742789
Andrew Serra <acs8929@rit.edu>
2018-12-03 19:39:34 -0500

    file migration

---
+++ 2018-2019/ProtoNEAT/Neat/neat_main.py
@@ -0,0 +1,14 @@
+import neat
+import os
+
+def eval_genomes(genomes, config):
+    pass
+
+def run(config):
+    pass
+
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config')
+    run(config_path)
---
+++ 2018-2019/ProtoNEAT/Neat/pacman.py
@@ -0,0 +1,5 @@
+from neat import DefaultGenome
+
+class Pacman(DefaultGenome):
+    def __init__(self):
+       pass
