d1c371c961a359f0b3202998f99093319cb8a90c
Prangon Das <pcd3897@g.rit.edu>
2019-02-09 15:34:48 -0500

    Pacneat can now restart and run the game still errors though

--- 2018-2019/ProtoNEAT/gameEngine/pacneat.py
+++ 2018-2019/ProtoNEAT/gameEngine/pacneat.py
@@ -1,47 +1,75 @@
 import os, sys, logging
 import neat
 import robomodules as rm
+import time
 from messages import *
-from pacbot.variables import *
+from pacbot.variables import game_frequency, ticks_per_update
+from pacbot import StateConverter, GameState
+
 
 
 
 ADDRESS = os.environ.get("BIND_ADDRESS","localhost")
 PORT = os.environ.get("BIND_PORT", 11297)
-
-SPEED = 1.0
-FREQUENCY = SPEED * game_frequency
+FREQUENCY = game_frequency * ticks_per_update
 
 
 num_generations = 300  # Number of generations
 
-class PacNeat(rm.ProtoModule):
+
+class PacEng(rm.ProtoModule):
 
     def __init__(self, addr, port):
-        self.subscriptions = [MsgType.FULL_STATE]
+        self.subscriptions = [MsgType.PACMAN_LOCATION]
         super().__init__(addr, port, message_buffers, MsgType, FREQUENCY, self.subscriptions)
-        self.state = None
+        self.game = GameState()
+        self.game.unpause()
 
+
+    def _write_state(self):
+        full_state = StateConverter.convert_game_state_to_full(self.game)
+        self.write(full_state.SerializeToString(), MsgType.FULL_STATE)
+
+        light_state = StateConverter.convert_game_state_to_light(self.game)
+        self.write(light_state.SerializeToString(), MsgType.LIGHT_STATE)
 
     def msg_received(self, msg, msg_type):
-        if msg_type == MsgType.FULL_STATE:
-            print(1)
-
-
+        if msg_type == MsgType.PACMAN_LOCATION:
+            self.game.pacbot.update((msg.x, msg.y))
 
     def tick(self):
-        pass
+        if self.game.lives < 3:
+            self.quit()
+        # this function will get called in a loop with FREQUENCY frequency
+        if self.game.play:
+            # update_pacbot_pos
+            # This will become asynchronous
+            self.game.next_step()
+        self._write_state()
+
+
+class PacNeat():
+
+    def __init__(self):
+        self.pacEng = PacEng(ADDRESS, PORT)
+
+
 
     # Called to evaluate all genomes
-    def eval_genomes(genomes, config):
+    def eval_genomes(self,genomes, config):
         for genome_id, genome in genomes:
-            pass
+            genome.fitness = 100
+            start = time.time()
+            self.pacEng.run()
+            duration = (time.time() - start)
+            genome.fitness -= duration
 
 
 
-    def runa(config_file):
+
+    def runa(self, config_file):
         # Get config_file and set up
-        config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultStagnation,
+        config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation,
                              config_file)
 
         # Creates the population
@@ -56,7 +84,7 @@ class PacNeat(rm.ProtoModule):
 
         # Run through generations
         # Call function to evaluate genomes in a generation
-        best_genome = pop.run(eval_genomes, num_generations)
+        best_genome = pop.run(self.eval_genomes, num_generations)
 
         # Display the best genome among all num_generations
         print("(Best genome: {0})".format(best_genome))
@@ -66,10 +94,13 @@ class PacNeat(rm.ProtoModule):
         # Best net
         best_net = neat.nn.FeedForwardNetwork.create(best_genome, config)
 
-
 def main():
-    module = PacNeat(ADDRESS, PORT)
-    module.run()
+    module = PacNeat()
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config')
+    module.runa(config_path)
+
+
 
 if __name__ == "__main__":
     main()
