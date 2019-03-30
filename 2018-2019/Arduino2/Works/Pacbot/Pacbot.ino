02feea480b3d978c89f28c420e5e17be4ef30fc1
Prangon Das <pcd3897@g.rit.edu>
2019-03-30 17:36:45 -0400

    goes straight

--- 2018-2019/Arduino2/Works/Pacbot/Pacbot.ino
+++ 2018-2019/Arduino2/Works/Pacbot/Pacbot.ino
@@ -1,32 +1,32 @@
 #include <PID_v1.h>
-#include <Servo.h> 
+#include <Servo.h>
 #include <Wire.h>
 #include <PID_AutoTune_v0.h>
 
 
 
 /**
- * Servo Motors
- */
+   Servo Motors
+*/
 Servo leftServo;
 Servo rightServo;
 
 /*
- * Sensor values
- */
+   Sensor values
+*/
 
 //PIN VALUES FOR SENSORS
 int rightSensor = A0;
 int leftSensor = A1;
 int frontSensor = A2;
 
-double rightInput;
-double leftInput;
-double frontInput;
+double rightInput = 0;
+double leftInput = 0;
+double frontInput = 0;
 
 /*
- * RPI values
- */
+   RPI values
+*/
 int rightRPI = 20;
 int leftRPI = 18;
 int upRPI = 16;
@@ -34,135 +34,50 @@ int downRPI = 14;
 
 int directionState = 0;
 
-//Define Variables we'll be connecting to
-double leftSetpoint, rightSetpoint = 180;
-double leftOutput = 93; 
-double rightOutput = 93;
-
-double kpL=2,kiL=0.5,kdL=2;
-double kpR=2,kiR=0.5,kdR=2;
-
-//Specify the links and initial tuning parameters
-PID leftPID(&leftInput, &rightOutput, &leftSetpoint, kpL,kiL,kdL, DIRECT);
-PID rightPID(&rightInput, &leftOutput, &rightSetpoint, kpR,kiR,kdR, DIRECT);
-PID_ATune leftATune(&leftInput, &leftOutput);
-PID_ATune rightATune(&rightInput, &rightOutput);
-
-byte ATuneModeRememberL=2;
-byte ATuneModeRememberR=2;
-double aTuneStep=50, aTuneNoise=1, aTuneStartValue=100;
-unsigned int aTuneLookBack=20;
-
-boolean tuningRight = true;
-boolean tuningLeft = true;
-
 void setup() {
   Serial.begin(9600);
-  
+
   leftServo.attach(9);
   rightServo.attach(10);
-  //Setup the pid 
-  leftPID.SetMode(AUTOMATIC);
-  rightPID.SetMode(AUTOMATIC);
-    if(tuningLeft)
-  {
-    tuningLeft=false;
-    changeAutoTune(leftATune, leftPID, tuningLeft);
-    tuningLeft=true;
-  }
-  if(tuningRight){
-    tuningRight=false;
-    changeAutoTune(rightATune, rightPID, tuningRight);
-    tuningRight=true;
-    }
-  
+}
+
+void adjustRight() {
+  leftServo.write(97);
+  rightServo.write(88);
+
+}
+
+void adjustLeft() {
+  leftServo.write(99);
+  rightServo.write(89);
 }
 
 void loop() {
-  //Serial.print(analogRead(rightSensor));
-  //Serial.print(" ");
-  Serial.println("Loop initiated");
-  if(tuningLeft)
-  {
-    Serial.println("Tuning Left");
-    byte valLeft = (leftATune.Runtime());
-    Serial.println(valLeft);
-    if (valLeft!=0)
-    {
-      Serial.println("RunTime done");
-      tuningLeft = false;
-    }
-   if(!tuningLeft)
-    {
-   //we're done, set the tuning parameters
-    kpL = leftATune.GetKp();
-    kiL = leftATune.GetKi();
-    kdL = leftATune.GetKd();
-    leftPID.SetTunings(kpL,kiL,kdL);
-          Serial.println("Setting new tunes done");
-    }
-  }else{
-          Serial.println("Computing PID");
-      leftPID.Compute();
-    }
-    if(tuningRight)
-  {
-    byte valRight = (rightATune.Runtime());
-    if (valRight!=0)
-    {
-      Serial.println("Tuning done");
-      tuningRight = false;
-    }
-   if(!tuningRight)
-    {
-   //we're done, set the tuning parameters
-    kpR = rightATune.GetKp();
-    kiR = rightATune.GetKi();
-    kdR = rightATune.GetKd();
-    rightPID.SetTunings(kpR,kiR,kdR);
-    }
-  }else{
-      rightPID.Compute();
-    }
-          Serial.println(leftOutput);
-    Serial.println(rightOutput);
-  //leftServo.write(leftOutput);
-  //rightServo.write(183-rightOutput);
+  Serial.print(analogRead(rightSensor));
+  Serial.print(" ");
+  Serial.println(analogRead(leftSensor));
 
-}
+  int readRight = analogRead(rightSensor);
+  int readLeft = analogRead(leftSensor);
 
-
-
-void changeAutoTune(PID_ATune ATune, PID pid, boolean tuning)
-{
- if(!tuning)
-  {
-    ATune.SetNoiseBand(aTuneNoise);
-    ATune.SetOutputStep(aTuneStep);
-    ATune.SetLookbackSec((int)aTuneLookBack);
-    if(tuning == tuningLeft){
-    AutoTuneHelper(true, pid, ATuneModeRememberL );
-    }else{
-     AutoTuneHelper(true, pid, ATuneModeRememberR);
-      }
-    tuning = true;
+  if (readRight > 235 && readRight < 350) {
+    Serial.print("stop right");
+    //delay(1000);
+    leftServo.write(93);
+    rightServo.write(93);
+    adjustRight();
   }
-  else
-  { //cancel autotune
-    ATune.Cancel();
-    tuning = false;
-    if(tuning == tuningLeft){
-    AutoTuneHelper(false, pid, ATuneModeRememberL );
-    }else{
-     AutoTuneHelper(false, pid, ATuneModeRememberR);
-      }
+  else if (readLeft > 225 && readLeft < 365 ) {
+    Serial.print("stop left");
+    //delay(1000);
+    leftServo.write(93);
+    rightServo.write(93);
+    adjustLeft();
   }
-}
+  else {
+    leftServo.write(180);
+    rightServo.write(0);
+  }
 
-void AutoTuneHelper(boolean start, PID pid, byte ATuneModeRemember)
-{
-  if(start)
-    ATuneModeRemember = pid.GetMode();
-  else
-    pid.SetMode(ATuneModeRemember);
+
 }
