e5380714a778277527b012ee7325769fd0f80792
Prangon Das <pcd3897@g.rit.edu>
2019-04-01 21:08:12 -0400

    Reintroduced gyro and wrote skeleton code

--- 2018-2019/Arduino2/Works/Pacbot/Pacbot.ino
+++ 2018-2019/Arduino2/Works/Pacbot/Pacbot.ino
@@ -1,9 +1,10 @@
-#include <PID_v1.h>
+#include <MPU6050.h>
 #include <Servo.h>
 #include <Wire.h>
-#include <PID_AutoTune_v0.h>
 #include <string.h>
 
+
+MPU6050 mpu;
 
 
 /**
@@ -28,21 +29,61 @@ double frontInput = 0;
 /*
    RPI values
 */
-int rightRPI = 20;
-int leftRPI = 18;
-int upRPI = 16;
-int downRPI = 14;
+int rightRPI = 13;
+int leftRPI = 12;
+int upRPI = 11;
+int downRPI = 8;
+int stopRPI = 4;
 
-int directionState = 0;
+/*
+ *  0 = up
+ *  1 = down
+ *  2 = left
+ *  3 = right
+ *  4 = stop
+ */
+int directionState = 0; 
+int queueState = 0;
+
 char *states[] = {"turnfull", "turnright", "turnleft", "stop", "straight"};
 char *currentState;
 int counter = 0; 
 
+double gyroStartVal;
+double gyroTurnedVal;
+double gyro180Val;
+
+boolean completedTurn = true;
+boolean completed180 = true;
+
+
+
 void setup() {
   Serial.begin(9600);
 
-  leftServo.attach(9);
-  rightServo.attach(10);
+  leftServo.attach(A0);
+  rightServo.attach(A1);
+  
+    // Initialize MPU6050
+  Serial.println("Initialize MPU6050");
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {   Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+
+   // If you want, you can set gyroscope offsets
+  // mpu.setGyroOffsetX(155);
+  // mpu.setGyroOffsetY(15);
+  // mpu.setGyroOffsetZ(15);
+  
+  // Calibrate gyroscope. The calibration must be at rest.
+  // If you don't want calibrate, comment this line.
+  mpu.calibrateGyro();
+
+  // Set threshold sensivty. Default 3.
+  // If you don't want use threshold, comment this line or set 0.
+  mpu.setThreshold(3);
+
 }
 
 
@@ -51,6 +92,25 @@ void adjustRight() {
   rightServo.write(87); //6 speed slow
 
 }
+
+void adjustTurnRight(){
+  leftServo.write(93);
+  rightServo.write(93);
+  }
+
+void adjustTurnLeft(){
+  leftServo.write(93);
+  rightServo.write(93); 
+  }
+
+void adjustForward(){
+  
+  }
+
+void adjustBackward(){
+  
+  }
+  
 void adjustLeft() {
   leftServo.write(99); //6 speed slow
   rightServo.write(89); //4 speed slow  
@@ -62,34 +122,75 @@ void stopMotors() {
 }
 
 void turnAround180() {
-  //delay(1000);
-  leftServo.write(180);
-  rightServo.write(180);
-  delay(1400);
+  turnAround90CW();
+  turnAround90CW();
 }
 
+
+boolean wallRight(){
+  
+  
+  }
+
+
+boolean wallLeft(){
+  
+  
+  }
+
+boolean wallFront(){
+  }
+
 void turnAround90CW() {
-  //delay(1000);
-  leftServo.write(180);
-  rightServo.write(180);
-  delay(800);  
+  while(mpu.readNormalizeGyro().XAxis < gyroTurnedVal){
+        if(wallRight()){
+          completedTurn = false;
+          break;}
+        leftServo.write(180);
+        rightServo.write(180);
+    }
+    if(!completedTurn){
+        adjustTurnLeft();
+        if(wallFront){
+        adjustForward();
+        }else{
+          adjustBackward();
+          }
+        turnAround90CW();
+        completedTurn = true;
+      }
 }
 
 void turnAround90CCW(){
-  //delay(1000);
-  leftServo.write(0);
-  rightServo.write(0);
-  delay(800);  
+  while(mpu.readNormalizeGyro().XAxis < gyroTurnedVal){
+        if(wallLeft()){
+          completedTurn = false;
+          break;}
+        leftServo.write(0);
+        rightServo.write(0);
+    }
+    if(!completedTurn){
+        adjustTurnRight();
+        if(wallFront){
+        adjustForward();
+        }else{
+          adjustBackward();
+          }
+        turnAround90CCW();
+        completedTurn = true;
+      };  
 }
 
+
+
 void moveStraight(int readRight, int readLeft) {
-  if (readRight > 280 && readRight < 350) {
+  if (wallRight()) {
     Serial.print("stop right");
     //delay(1000);
     stopMotors();
     adjustRight();
   }
-  else if (readLeft > 225 && readLeft < 365 ) {
+  else if (wallLeft() ) {
     Serial.print("stop left");
     //delay(1000);
     stopMotors(); 
@@ -110,8 +211,41 @@ void loop() {
   Serial.print(" ");
   Serial.println(analogRead(leftSensor));
 
+
+  if(digitalRead(upRPI) == HIGH ){
+      if(directionState != 0){
+          
+      }
+
+    
+    }else if(digitalRead(downRPI) ==HIGH){
+        if(directionState != 1){
+          
+        }
+
+
+      
+      }else if(digitalRead(leftRPI) == HIGH){
+        if(directionState != 2){
+          
+        }
+
+
+        
+        }else if(digitalRead(rightRPI) == HIGH){
+            if(directionState != 3){
+          
+            }
+
+          
+          }else{
+
+            
+            }
+            
   int readRight = analogRead(rightSensor);
   int readLeft = analogRead(leftSensor);
+  int readFront = analogRead(frontSensor);
 
   //straight
   if (strcmp(currentState, states[4]) == 0) {
@@ -139,4 +273,9 @@ void loop() {
   if(counter < 4){
     counter += 1;
   }
+
+  
+
+
+  
 }
