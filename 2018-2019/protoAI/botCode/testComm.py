2a4898e65551195a249edc302a08b267d0afe01d
Prangon Das <pcd3897@g.rit.edu>
2019-04-05 15:48:19 -0400

    updates

--- 2018-2019/protoAI/botCode/testComm.py
+++ 2018-2019/protoAI/botCode/testComm.py
@@ -11,6 +11,7 @@ def __init__(self):
     GPIO.setup(35, GPIO.OUT)
     GPIO.setup(33, GPIO.OUT)
     GPIO.setup(31, GPIO.OUT)
+    GPIO.setup(29, GPIO.OUT)
 
 def print_direction( value):
     if (value == 0):
@@ -19,37 +20,44 @@ def print_direction( value):
         GPIO.output(35, GPIO.LOW)
         GPIO.output(33, GPIO.LOW)
         GPIO.output(31, GPIO.LOW)
+        GPIO.output(29, GPIO.LOW)
     elif (value == 1):
         print("Moving Left")
         GPIO.output(37, GPIO.LOW)
         GPIO.output(35, GPIO.HIGH)
         GPIO.output(33, GPIO.LOW)
         GPIO.output(31, GPIO.LOW)
+        GPIO.output(29, GPIO.LOW)
     elif (value == 2):
         print("Moving Up")
         GPIO.output(37, GPIO.LOW)
         GPIO.output(35, GPIO.LOW)
         GPIO.output(33, GPIO.HIGH)
         GPIO.output(31, GPIO.LOW)
+        GPIO.output(29, GPIO.LOW)
     elif (value == 3):
         print("Moving Down")
         GPIO.output(37, GPIO.LOW)
         GPIO.output(35, GPIO.LOW)
         GPIO.output(33, GPIO.LOW)
         GPIO.output(31, GPIO.HIGH)
+        GPIO.output(29, GPIO.LOW)
     elif (value == 4):
         print("Stop")
         GPIO.output(37, GPIO.LOW)
         GPIO.output(35, GPIO.LOW)
         GPIO.output(33, GPIO.LOW)
-        GPIO.output(31, GPIO.HIGH)
+        GPIO.output(31, GPIO.LOW)
+        GPIO.output(29, GPIO.HIGH)
 
 
 
 
 def main():
     try:
-        print_direction(2)
+        while(1):
+            print_direction(2)
+
     finally:
         GPIO.cleanup()
 
