28c2562feacef2f257379b5288e7c70b9f43ab17
Michael Elia <michaeljelia@gmail.com>
2021-09-25 15:29:56 -0400

    + Fixed directories in pacbotNoVisToFile.sh

--- 2021-2022/linux/pacbotNoVisToFile.sh
+++ 2021-2022/linux/pacbotNoVisToFile.sh
@@ -1,7 +1,7 @@
 cd ../
-python3 gameEngine/server.py > tests/currenttest/GameServer.txt &
-python3 botCode/server.py > tests/currenttest/BotServer.txt &
+python3 gameEngine/server.py > linux/tests/currenttest/GameServer.txt &
+python3 botCode/server.py > linux/tests/currenttest/BotServer.txt &
 sleep 1
-python3 botCode/pacbotCommsModule.py > tests/currenttest/botComms.txt &
-python3 botCode/highLevelPacman.py > tests/currenttest/Pacman.txt &
-python3 gameEngine/gameEngine.py > tests/currenttest/GameEngine.txt
+python3 botCode/pacbotCommsModule.py > linux/tests/currenttest/botComms.txt &
+python3 botCode/highLevelPacman.py > linux/tests/currenttest/Pacman.txt &
+python3 gameEngine/gameEngine.py > linux/tests/currenttest/GameEngine.txt
