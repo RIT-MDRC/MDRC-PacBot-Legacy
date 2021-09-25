55bbb933f0284e0c710b0d634e5d2ce7d930e33e
Chidera <cme4677@g.rit.edu>
2021-09-25 15:44:10 -0400

    changed sleep duration from 1 to 0.5s

--- 2021-2022/linux/pacbotNoVis.sh
+++ 2021-2022/linux/pacbotNoVis.sh
@@ -1,7 +1,7 @@
 cd ../
 python3 gameEngine/server.py &
 python3 botCode/server.py &
-sleep 1
+sleep 0.5
 python3 botCode/pacbotCommsModule.py &
 python3 botCode/highLevelPacman.py &
 python3 gameEngine/gameEngine.py
