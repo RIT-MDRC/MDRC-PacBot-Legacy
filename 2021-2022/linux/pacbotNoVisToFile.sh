63675aa4d07721f441c08e8a05f8a1a8028a0a95
Michael Elia <michaeljelia@gmail.com>
2021-10-02 14:13:19 -0400

    + Edited pacbotNoVisToFile.sh with -u flag to instantly write to text files

--- 2021-2022/linux/pacbotNoVisToFile.sh
+++ 2021-2022/linux/pacbotNoVisToFile.sh
@@ -1,10 +1,10 @@
 cd ../
 mkdir linux/tests
 mkdir linux/tests/currenttest
-python3 gameEngine/server.py > linux/tests/currenttest/GameServer.txt &
-python3 botCode/server.py > linux/tests/currenttest/BotServer.txt &
+python3 -u gameEngine/server.py > linux/tests/currenttest/GameServer.txt &
+python3 -u botCode/server.py > linux/tests/currenttest/BotServer.txt &
 sleep 1
-python3 botCode/pacbotCommsModule.py > linux/tests/currenttest/botComms.txt &
-python3 botCode/highLevelPacman.py > linux/tests/currenttest/Pacman.txt &
-python3 gameEngine/gameEngine.py > linux/tests/currenttest/GameEngine.txt
+python3 -u botCode/pacbotCommsModule.py > linux/tests/currenttest/botComms.txt &
+python3 -u botCode/highLevelPacman.py > linux/tests/currenttest/Pacman.txt &
+python3 -u gameEngine/gameEngine.py > linux/tests/currenttest/GameEngine.txt
 sleep 5
