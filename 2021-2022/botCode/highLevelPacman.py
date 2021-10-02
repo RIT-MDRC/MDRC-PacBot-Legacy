313e0aeb2c34bd909e0d93d434b69568bc316cf4
Chidera <cme4677@g.rit.edu>
2021-10-02 15:40:42 -0400

    type casted to int on lines 30-35

--- 2021-2022/botCode/highLevelPacman.py
+++ 2021-2022/botCode/highLevelPacman.py
@@ -27,12 +27,12 @@ class highLevelPacman(rm.ProtoModule):
         with open("botCode/weights.txt", "r") as f:
             lines = f.readlines()
             values = lines[0].split()
-            FREQUENCY = values[0]
-            FEAR = values[1]
-            PELLET_WEIGHT = values[2]
-            SUPER_PELLET_WEIGHT = values[3]
-            GHOST_WEIGHT = values[4]
-            FRIGHTENED_GHOST_WEIGHT = values[5]
+            FREQUENCY = int(values[0])
+            FEAR = int(values[1])
+            PELLET_WEIGHT = int(values[2])
+            SUPER_PELLET_WEIGHT = int(values[3])
+            GHOST_WEIGHT = int(values[4])
+            FRIGHTENED_GHOST_WEIGHT = int(values[5])
             print(FREQUENCY)
 
         super().__init__(addr, port, message_buffers, MsgType, FREQUENCY, self.subscriptions)
