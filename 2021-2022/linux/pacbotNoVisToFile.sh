18cdcc5d305fdf2a09db8991195f58a97161ed52
Michael Elia <michaeljelia@gmail.com>
2021-10-02 14:26:27 -0400

    + Edited pacbotNoVisToFile.sh to actually start the simulation - it works now as a bash file

--- 2021-2022/linux/pacbotNoVisToFile.sh
+++ 2021-2022/linux/pacbotNoVisToFile.sh
@@ -1,10 +1,9 @@
 cd ../
-mkdir linux/tests
-mkdir linux/tests/currenttest
+mkdir -p linux/tests/currenttest
 python3 -u gameEngine/server.py > linux/tests/currenttest/GameServer.txt &
 python3 -u botCode/server.py > linux/tests/currenttest/BotServer.txt &
 sleep 1
 python3 -u botCode/pacbotCommsModule.py > linux/tests/currenttest/botComms.txt &
 python3 -u botCode/highLevelPacman.py > linux/tests/currenttest/Pacman.txt &
-python3 -u gameEngine/gameEngine.py > linux/tests/currenttest/GameEngine.txt
+python3 -u gameEngine/gameEngine.py << linux/p.txt > linux/tests/currenttest/GameEngine.txt
 sleep 5
