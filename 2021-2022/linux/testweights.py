07dc5abee11319ded83c64cb83a07c49bcd462f6
Michael Elia <michaeljelia@gmail.com>
2021-10-02 15:34:29 -0400

    Added loop to read lines in test_weights.txt

--- 2021-2022/linux/testweights.py
+++ 2021-2022/linux/testweights.py
@@ -1,2 +1,9 @@
 import subprocess
-subprocess.call(["sh", "pacbotNoVisToFile.sh"])
+
+with open("test_weights.txt", "r") as f:
+    lines = f.readlines()
+    for line in lines:
+        weights = line.split(' ')
+        print(','.join(weights))
+        subprocess.call(["sh", "pacbotNoVisToFile.sh"])
+
