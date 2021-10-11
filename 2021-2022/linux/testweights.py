f0a22c66d7e2df603a3487192df5cf3ca2212ae6
Michael Elia <michaeljelia@gmail.com>
2021-10-11 16:03:02 -0400

    Fixed command not in list form

--- 2021-2022/linux/testweights.py
+++ 2021-2022/linux/testweights.py
@@ -13,7 +13,7 @@ with open("test_weights.txt", "r") as f:
             score = 0
             print('creating subprocess')
             # process = subprocess.Popen("sh pacbotNoVis.sh", shell=True, stdout=subprocess.PIPE)
-            process = subprocess.run("sh pacbotNoVis.sh", capture_output=True, timeout=20)
+            process = subprocess.run(["sh", "pacbotNoVis.sh"], capture_output=True, timeout=20)
             for processLine in process.stdout:
                 if processLine[:7] == 'score: ':
                     score = processLine[7:]
