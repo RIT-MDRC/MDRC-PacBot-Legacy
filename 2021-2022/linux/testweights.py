0a983f9b9003eff579ecda8a3c6cfca1cd4e2f23
Michael Elia <michaeljelia@gmail.com>
2021-10-11 16:02:21 -0400

    Uses run and a timeout

--- 2021-2022/linux/testweights.py
+++ 2021-2022/linux/testweights.py
@@ -12,18 +12,22 @@ with open("test_weights.txt", "r") as f:
                 f.write(' '.join(weights))
             score = 0
             print('creating subprocess')
-            process = subprocess.Popen("sh pacbotNoVis.sh", shell=True, stdout=subprocess.PIPE)
+            # process = subprocess.Popen("sh pacbotNoVis.sh", shell=True, stdout=subprocess.PIPE)
+            process = subprocess.run("sh pacbotNoVis.sh", capture_output=True, timeout=20)
+            for processLine in process.stdout:
+                if processLine[:7] == 'score: ':
+                    score = processLine[7:]
             print('subprocess created')
-            try:
-                outs, errs = process.communicate(timeout=5)
-                for processLine in outs:
-                    print(processLine)
-                    if processLine == 'Stop\n':
-                        process.kill()
-                    elif processLine[:7] == 'score: ':
-                        score = processLine[7:]
-            except subprocess.TimeoutExpired:
-                process.kill()
+            # try:
+            #     outs, errs = process.communicate(timeout=5)
+            #     for processLine in outs:
+            #         print(processLine)
+            #         if processLine == 'Stop\n':
+            #             process.kill()
+            #         elif processLine[:7] == 'score: ':
+            #             score = processLine[7:]
+            # except subprocess.TimeoutExpired:
+            #     process.kill()
             print('subprocess complete: score: ' + str(score))
             # for processLineBinary in process.stdout:
             #     processLine = processLineBinary.decode('ascii')
