2ee2352f458d8222a398240d60fd8ce80157dcc7
Michael Elia <michaeljelia@gmail.com>
2021-10-11 15:34:59 -0400

    Uses communicate instead of for in

--- 2021-2022/linux/testweights.py
+++ 2021-2022/linux/testweights.py
@@ -11,14 +11,27 @@ with open("test_weights.txt", "r") as f:
             with open("../botCode/weights.txt", "w") as f:
                 f.write(' '.join(weights))
             score = 0
+            print('creating subprocess')
             process = subprocess.Popen("sh pacbotNoVis.sh", shell=True, stdout=subprocess.PIPE)
-            for processLineBinary in process.stdout:
-                processLine = processLineBinary.decode('ascii')
-                print(processLine)
-                if processLine == 'Stop\n':
-                    process.terminate()
-                elif processLine[:7] == 'score: ':
-                    score = processLine[7:]
+            print('subprocess created')
+            try:
+                outs, errs = process.communicate(timeout=5)
+                for processLine in outs:
+                    print(processLine)
+                    if processLine == 'Stop\n':
+                        process.kill()
+                    elif processLine[:7] == 'score: ':
+                        score = processLine[7:]
+            except subprocess.TimeoutExpired:
+                process.kill()
+            print('subprocess complete: score: ' + score)
+            # for processLineBinary in process.stdout:
+            #     processLine = processLineBinary.decode('ascii')
+            #     print(processLine)
+            #     if processLine == 'Stop\n':
+            #         process.terminate()
+            #     elif processLine[:7] == 'score: ':
+            #         score = processLine[7:]
             results.append(score)
 
 print(results)
