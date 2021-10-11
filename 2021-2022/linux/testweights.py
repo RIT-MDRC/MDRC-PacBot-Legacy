7371693cac45f89a780e5126e27d56841d06da11
Michael Elia <michaeljelia@gmail.com>
2021-10-11 14:59:22 -0400

    Updates test_weights.txt to add more iterations, updated testweights.py to kill process when it says "stop" (when pacbot dies) and print out the score for each iteration

--- 2021-2022/linux/testweights.py
+++ 2021-2022/linux/testweights.py
@@ -1,5 +1,7 @@
 import subprocess
 
+results = []
+
 with open("test_weights.txt", "r") as f:
     lines = f.readlines()
     for line in lines:
@@ -7,5 +9,17 @@ with open("test_weights.txt", "r") as f:
         print(','.join(weights))
         with open("../botCode/weights.txt", "w") as f:
             f.write(' '.join(weights))
+        score = 0
         process = subprocess.Popen("sh pacbotNoVisToFile.sh", shell=True, stdout=subprocess.PIPE)
+        for processLine in process.stdout:
+            print(processLine)
+            if processLine == 'Stop':
+                process.kill()
+                break
+            elif processLine[:7] == 'score: ':
+                score = processLine[7:]
+        results.append(score)
+
+print(results)
+
 
