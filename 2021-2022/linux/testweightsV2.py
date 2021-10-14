ad01f1237922378c22bb371aa2ee6657a94dba42
MJE10 <michaeljelia@gmail.com>
2021-10-13 21:08:09 -0400

    Checks ports before attempting to launch games on them in an effort to reduce collisions (estimated 29% of ports collided last time)

--- 2021-2022/linux/testweightsV2.py
+++ 2021-2022/linux/testweightsV2.py
@@ -2,9 +2,10 @@ import subprocess
 import sys
 import time
 from datetime import datetime
+import socket
 
 results = 0
-ports = [11295, 12295]
+ports = [11295, 15295]
 
 with open("test_weights.txt", "r") as f:
     lines = f.readlines()
@@ -23,6 +24,23 @@ with open("test_weights.txt", "r") as f:
 
                 ports[0] += 1
                 ports[1] += 1
+
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                result = sock.connect_ex(('127.0.0.1', ports[0]))
+                result1 = sock.connect_ex(('127.0.0.1', ports[1]))
+                if result != 0 or result1 != 0:
+                    sock.close()
+                    sock1.close()
+                    ports[0] += 1
+                    ports[1] += 1
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    result = sock.connect_ex(('127.0.0.1', ports[0]))
+                    result1 = sock.connect_ex(('127.0.0.1', ports[1]))
+                sock.close()
+                sock1.close()
+
                 time.sleep(0.5)
 
     print('All processes initialized.')
