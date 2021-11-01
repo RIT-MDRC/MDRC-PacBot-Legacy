b3ad10d86e84cfbc6c9f8ba60df54662fbf259e2
Michael Elia <michaeljelia@gmail.com>
2021-11-01 15:55:27 -0400

    rewrote testweightsV3.py intended to be run once but takes from google sheet

--- 2021-2022/linux/testweightsV3.py
+++ 2021-2022/linux/testweightsV3.py
@@ -4,6 +4,7 @@ import subprocess
 import math
 import os.path
 import random
+import sys
 import time
 import socket
 
@@ -12,187 +13,116 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-# The ID and range of a sample spreadsheet.
-SPREADSHEET_ID = '1QDQBckl58-aCvUn4bPmHVkHH8CDi7GZy8oOo-0AxJU4'
+class TestWeights:
+    def __init__(self):
+        # If modifying these scopes, delete the file token.json.
+        self.SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-spreadsheetInfo = {
-    'last_cached': 0.0,
-    'status': 'run',
-    'max_processes': 1,
-    'weight_sets': [[''], []]
-}
+        # The ID of the spreadsheet.
+        self.SPREADSHEET_ID = '1QDQBckl58-aCvUn4bPmHVkHH8CDi7GZy8oOo-0AxJU4'
 
-def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+        self.weight_sets = []
+        self.last_cached = 0.0
+        self.max_processes = 1
+
+        self.sheet = None
+
+        self.initGoogle()
+        self.getFromGoogle()
+
+        self.processes = {}
+        self.ports = [11295, 15295]
+
+        while len(self.processes) < self.max_processes:
+            self.initProcess(self.weight_sets[random.randint(0, len(self.weight_sets))])
+
+        while len(self.processes) > 0:
+            self.finishProcesses()
+            # if time.time() > self.last_cached + 30:
+            #     self.getFromGoogle()
+            time.sleep(10)
+
+        if self.max_processes == 0:
+            sys.exit(1)
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+            sys.exit(0)
 
-    service = build('sheets', 'v4', credentials=creds)
-    sheet = service.spreadsheets()
-    # appending to sheet test
-    # sheet = service.spreadsheets()
-    # result = sheet.values().append(spreadsheetId=SPREADSHEET_ID, range="Results", valueInputOption="USER_ENTERED", body=({'majorDimension': 'ROWS', 'values': [[1,2,3,4,5,6]+[2]]})).execute()
+    def initGoogle(self):
+        credentials = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            credentials = Credentials.from_authorized_user_file('token.json', self.SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not credentials or not credentials.valid:
+            if credentials and credentials.expired and credentials.refresh_token:
+                credentials.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.SCOPES)
+                credentials = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(credentials.to_json())
 
-    def getRange(myRange):
+        service = build('sheets', 'v4', credentials=credentials)
+        self.sheet = service.spreadsheets()
+
+    def getFromGoogle(self):
+        self.last_cached = time.time()
+        values = self.getRange('Dashboard!A:B')
+        self.max_processes = min(values[3][0], values[5][0])
+        self.weight_sets = [v[1] for v in values[1:] if len(values) >= 2]
+
+    def getRange(self, my_range):
         while 1:
             try:
                 # Call the Sheets API
-                result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=myRange).execute()
+                result = self.sheet.values().get(spreadsheetId=self.SPREADSHEET_ID, range=my_range).execute()
                 return result.get('values', [])
             except:
-                print('Failed to get range ' + myRange + '. This is likely a Google error, possibly related to rate limiting. There might also be a problem with your internet connection. Sleeping for 10 seconds and trying again.')
+                print('Failed to get range ' + my_range + '. This is likely a Google error, possibly related to rate '
+                                                          'limiting. There might also be a problem with your internet '
+                                                          'connection. Sleeping for 10 seconds and trying again.')
                 time.sleep(10)
 
-    processes = {}
-    results = 0
-    ports = [11295, 15295]
+    def initProcess(self, weights):
+        with open("../botCode/weights.txt", "w") as f:
+            f.write(' '.join(weights))
+        self.ports[0] += 1
+        self.ports[1] += 1
+        # print('6) new ports found and saved')
+        self.processes[str(self.ports[0])] = [weights,
+                                              subprocess.Popen(["sh", "pacbotNoVisToFileV3.sh", str(self.ports[0])],
+                                                               stdout=subprocess.DEVNULL),
+                                              subprocess.Popen(
+                                                  ["python3", "-u", "../gameEngine/server.py", str(self.ports[0]),
+                                                   str(self.ports[1])])
+                                              ]
+        time.sleep(10)
+        self.processes[str(self.ports[0])].append(subprocess.Popen(
+            ["python3", "-u", "../botCode/server.py", str(self.ports[0]),
+             str(self.ports[1])], stdout=subprocess.DEVNULL))
+        self.processes[str(self.ports[0])].append(subprocess.Popen(
+            ["python3", "-u", "../botCode/pacbotCommsModule.py", str(self.ports[0]),
+             str(self.ports[1])], stdout=subprocess.DEVNULL))
+        self.processes[str(self.ports[0])].append(subprocess.Popen(
+            ["python3", "-u", "botCode/highLevelPacman.py",
+             str(self.ports[0]),
+             str(self.ports[1]), ">", "linux/tests/currenttest_$1/Pacman.txt"], cwd="../", stdout=subprocess.DEVNULL))
+        self.processes[str(self.ports[0])].append(subprocess.Popen(
+            ["python3", "-u", "../gameEngine/gameEngine.py",
+             str(self.ports[0]),
+             str(self.ports[1]), "<<", "../linux/p.txt"], stdout=subprocess.DEVNULL))
 
-    while 1:
-        print('1) processes length: ' + str(len(processes.keys())))
-
-        if time.time() > spreadsheetInfo['last_cached'] + 30:
-            spreadsheetInfo['last_cached'] = time.time()
-            values = getRange('Dashboard!A:B')
-            # spreadsheetInfo['status'] = getRange('Dashboard!A2:A2')
-            # spreadsheetInfo['max_processes'] = getRange('Dashboard!A4:A4')
-            # spreadsheetInfo['weight_sets'] = getRange('Dashboard!B:B')
-            spreadsheetInfo['status'] = values[1][0]
-            spreadsheetInfo['max_processes'] = values[3][0]
-            spreadsheetInfo['weight_sets'] = [v[1] for v in values if len(values) >= 2]
-
-        print('2) data acquired')
-
-        if spreadsheetInfo['status'] == 'stop':
-            print('The dashboard has indicated that I should stop. Goodbye!')
-            break
-        elif spreadsheetInfo['status'] == 'run':
-            if len(processes.keys()) >= int(spreadsheetInfo['max_processes']):
-                print('3) Maximum number of processes (' + str(spreadsheetInfo['max_processes']) + ') already running.')
-            else:
-                print('3) Starting new weight set...')
-                values = spreadsheetInfo['weight_sets']
-                if not values:
-                    print('No data found.')
-                else:
-                    rowNum = math.floor(random.random()*(len(values)-1)) + 1
-                    weights = values[rowNum].split(' ')
-                    if len(weights) == 6:
-                        print('5) Testing weight set: ' + ', '.join(weights))
-
-                        while 1:
-                            try:
-                                with open("../botCode/weights.txt", "w") as f:
-                                    f.write(' '.join(weights))
-                                break
-                            except:
-                                print('Error opening bot/weights.txt file. Sleeping for 5 seconds then trying again.')
-                                time.sleep(5)
-
-                        while 1:
-                            try:
-                                ports[0] += 1
-                                ports[1] += 1
-
-                                while 1:
-                                    try:
-                                        # find new ports for next time around
-                                        # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                                        # sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                                        # result = sock.connect_ex(('localhost', ports[0]))
-                                        # result1 = sock.connect_ex(('localhost', ports[1]))
-                                        # while result != 0 or result1 != 0:
-                                        #     print("socket error code: " + str(result))
-                                        #     print("socket error code (111=port taken): " + str(result1))
-                                        #     sock.close()
-                                        #     sock1.close()
-                                        #     ports[0] += 1
-                                        #     ports[1] += 1
-                                        #     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                                        #     sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                                        #     result = sock.connect_ex(('localhost', ports[0]))
-                                        #     result1 = sock2.connect_ex(('localhost', ports[1]))
-                                        # sock.close()
-                                        # sock1.close()
-                                        break
-                                    except Exception as e:
-                                        print(
-                                            'Socket port connecting failed. Incrementing ports and trying again in 5 seconds.')
-                                        print(e)
-                                        ports[0] += 1
-                                        ports[1] += 1
-                                        time.sleep(5)
-
-                                print('6) new ports found and saved')
-                                # processes[str(ports[0])] = [weights, subprocess.Popen(
-                                #     ["sh", "pacbotNoVisToFileV2.sh", str(ports[0]), str(ports[1])],
-                                #     stdout=subprocess.DEVNULL)]
-                                processes[str(ports[0])] = [weights,
-                                                            subprocess.Popen(["sh", "pacbotNoVisToFileV3.sh", str(ports[0])], stdout=subprocess.DEVNULL),
-                                                            subprocess.Popen(["python3", "-u", "../gameEngine/server.py", str(ports[0]), str(ports[1])])
-                                ]
-                                time.sleep(10)
-                                processes[str(ports[0])].append(subprocess.Popen(
-                                                                ["python3", "-u", "../botCode/server.py", str(ports[0]),
-                                                                 str(ports[1])], stdout=subprocess.DEVNULL))
-                                processes[str(ports[0])].append(subprocess.Popen(
-                                                                ["python3", "-u", "../botCode/pacbotCommsModule.py", str(ports[0]),
-                                                                 str(ports[1])], stdout=subprocess.DEVNULL))
-                                processes[str(ports[0])].append(subprocess.Popen(
-                                                                ["python3", "-u", "botCode/highLevelPacman.py",
-                                                                 str(ports[0]),
-                                                                 str(ports[1]), ">", "linux/tests/currenttest_$1/Pacman.txt"], cwd="../", stdout=subprocess.DEVNULL))
-                                processes[str(ports[0])].append(subprocess.Popen(
-                                                                ["python3", "-u", "../gameEngine/gameEngine.py",
-                                                                 str(ports[0]),
-                                                                 str(ports[1]), "<<", "../linux/p.txt"], stdout=subprocess.DEVNULL))
-                                break
-                            except Exception as e:
-                                for i in range(1, len(processes[str(ports[0])]) - 1):
-                                    try:
-                                        processes[str(ports[0])][i].kill()
-                                        processes[str(ports[0])][i].wait()
-                                    except:
-                                        pass
-                                print('Error opening process. Sleeping for 5 seconds then trying again.')
-                                print(e)
-                                time.sleep(5)
-                        print('7) Process initializing... locating new sockets')
-    
-
-                    else:
-                        print('Invalid weight set: ' + str(weights[0]))
-        elif spreadsheetInfo['status'] == 'pause':
-            print('Paused...')
-        else:
-            print('Invalid instruction "' + str(spreadsheetInfo['status']) + '" should be either "run" or "stop"')
-
-        print('8) checking for results and terminating completed processes...')
-        # check for results
+    def finishProcesses(self):
         completedProcesses = {}
-        for process in processes:
+        for process in self.processes:
             consecutiveStopCount = -1
             try:
-                with open("tests/currenttest_"+process+"/Pacman.txt", "r") as pacmantxt:
+                with open("tests/currenttest_" + process + "/Pacman.txt", "r") as pacmantxt:
                     pacmantxtlines = pacmantxt.readlines()[1:]
                 for pacmantxtline in pacmantxtlines:
                     if pacmantxtline == 'Stop\n':
@@ -201,12 +131,13 @@ def main():
                     else:
                         consecutiveStopCount = 0
                 if consecutiveStopCount >= 10:
-                    for i in range(1, len(processes[process]) - 1):
-                        processes[process][i].kill()
-                        processes[process][i].wait()
-                    completedProcesses[process] = processes[process]
+                    for i in range(1, len(self.processes[process]) - 1):
+                        self.processes[process][i].kill()
+                        self.processes[process][i].wait()
+                    completedProcesses[process] = self.processes[process]
             except:
                 # game hasn't started yet
+                print('Not found: ' + "tests/currenttest_" + process + "/Pacman.txt")
                 pass
         print('9) submitting scores...')
         for process in completedProcesses:
@@ -220,24 +151,26 @@ def main():
                                 score = str(int(processLine[7:]))
                     break
                 except:
-                    print('Error reading ' + "tests/currenttest_" + process + "/Pacman.txt" + "; trying again in 5 seconds")
+                    print(
+                        'Error reading ' + "tests/currenttest_" + process + "/Pacman.txt" + "; trying again in 5 seconds")
                     time.sleep(5)
 
             # submit score to google sheets
             while 1:
                 try:
-                    sheet = service.spreadsheets()
-                    result = sheet.values().append(spreadsheetId=SPREADSHEET_ID, range="Results",
-                                                   valueInputOption="USER_ENTERED", body=(
-                        {'majorDimension': 'ROWS', 'values': [completedProcesses[process][0] + [score]]})).execute()
-                    processes.pop(process)
+                    result = self.sheet.values().append(spreadsheetId=self.SPREADSHEET_ID, range="Results",
+                                                        valueInputOption="USER_ENTERED", body=(
+                            {'majorDimension': 'ROWS', 'values': [completedProcesses[process][0] + [score]]})).execute()
+                    self.processes.pop(process)
                     break
                 except:
                     print('Error submitting results, trying again in 10 seconds.')
                     time.sleep(10)
 
-        print('10) done')
-        time.sleep(1)
+
+def main():
+    x = TestWeights()
+
 
 if __name__ == '__main__':
     main()
