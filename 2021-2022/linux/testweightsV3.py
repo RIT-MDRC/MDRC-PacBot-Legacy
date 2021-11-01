b85d4ec6806a6ba8f9e5a73a049fc8e358984249
MJE10 <michaeljelia@gmail.com>
2021-10-31 23:37:14 -0400

    Removed ../ from hlp call because already root

--- 2021-2022/linux/testweightsV3.py
+++ 2021-2022/linux/testweightsV3.py
@@ -122,9 +122,9 @@ def main():
                                                                 ["python3", "-u", "../botCode/pacbotCommsModule.py", str(ports[0]),
                                                                  str(ports[1])]))
                                 processes[str(ports[0])].append(subprocess.Popen(
-                                                                ["python3", "-u", "../botCode/highLevelPacman.py",
+                                                                ["python3", "-u", "botCode/highLevelPacman.py",
                                                                  str(ports[0]),
-                                                                 str(ports[1]), ">", "../linux/tests/currenttest_$1/Pacman.txt"], cwd="../"))
+                                                                 str(ports[1]), ">", "linux/tests/currenttest_$1/Pacman.txt"], cwd="../"))
                                 processes[str(ports[0])].append(subprocess.Popen(
                                                                 ["python3", "-u", "../gameEngine/gameEngine.py",
                                                                  str(ports[0]),
