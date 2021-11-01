08377229b4e1514f338a2cbfa59efd09c3b8dd6a
MJE10 <michaeljelia@gmail.com>
2021-10-31 23:30:24 -0400

    Added (cd ../;) to pacbotCommsModule call so that it is started from the correct root folder

--- 2021-2022/linux/testweightsV3.py
+++ 2021-2022/linux/testweightsV3.py
@@ -119,8 +119,8 @@ def main():
                                                                 ["python3", "-u", "../botCode/server.py", str(ports[0]),
                                                                  str(ports[1])]))
                                 processes[str(ports[0])].append(subprocess.Popen(
-                                                                ["python3", "-u", "../botCode/pacbotCommsModule.py", str(ports[0]),
-                                                                 str(ports[1])]))
+                                                                ["(cd", "../;", "python3", "-u", "../botCode/pacbotCommsModule.py", str(ports[0]),
+                                                                 str(ports[1])+")"]))
                                 processes[str(ports[0])].append(subprocess.Popen(
                                                                 ["(cd", "../;", "python3", "-u", "../botCode/highLevelPacman.py",
                                                                  str(ports[0]),
