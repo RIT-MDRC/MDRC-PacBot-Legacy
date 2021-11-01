8b32a75f2ff1b99a4574ff095ac5b177e2c9327f
MJE10 <michaeljelia@gmail.com>
2021-10-31 23:22:11 -0400

    Added ../ to get into root folder

--- 2021-2022/linux/testweightsV3.py
+++ 2021-2022/linux/testweightsV3.py
@@ -110,26 +110,25 @@ def main():
 
                         while 1:
                             try:
-                                subprocess.Popen(["ls"], stdout=subprocess.DEVNULL)
                                 processes[str(ports[0])] = [weights,
                                                             subprocess.Popen(["sh", "pacbotNoVisToFileV3.sh", str(ports[0])], stdout=subprocess.DEVNULL),
-                                                            subprocess.Popen(["python3", "-u", "gameEngine/server.py", str(ports[0]), str(ports[1])])
+                                                            subprocess.Popen(["python3", "-u", "../gameEngine/server.py", str(ports[0]), str(ports[1])])
                                 ]
                                 time.sleep(5)
                                 processes[str(ports[0])].append(subprocess.Popen(
-                                                                ["python3", "-u", "botCode/server.py", str(ports[0]),
+                                                                ["python3", "-u", "../botCode/server.py", str(ports[0]),
                                                                  str(ports[1])]))
                                 processes[str(ports[0])].append(subprocess.Popen(
-                                                                ["python3", "-u", "botCode/pacbotCommsModule.py", str(ports[0]),
+                                                                ["python3", "-u", "../botCode/pacbotCommsModule.py", str(ports[0]),
                                                                  str(ports[1])]))
                                 processes[str(ports[0])].append(subprocess.Popen(
-                                                                ["python3", "-u", "botCode/highLevelPacman.py",
+                                                                ["python3", "-u", "../botCode/highLevelPacman.py",
                                                                  str(ports[0]),
-                                                                 str(ports[1]), ">", "linux/tests/currenttest_$1/Pacman.txt"]))
+                                                                 str(ports[1]), ">", "../linux/tests/currenttest_$1/Pacman.txt"]))
                                 processes[str(ports[0])].append(subprocess.Popen(
-                                                                ["python3", "-u", "gameEngine/gameEngine.py",
+                                                                ["python3", "-u", "../gameEngine/gameEngine.py",
                                                                  str(ports[0]),
-                                                                 str(ports[1]), "<<", "linux/p.txt"]))
+                                                                 str(ports[1]), "<<", "../linux/p.txt"]))
                                 break
                             except:
                                 for i in range(1, len(processes[str(ports[0])]) - 1):
