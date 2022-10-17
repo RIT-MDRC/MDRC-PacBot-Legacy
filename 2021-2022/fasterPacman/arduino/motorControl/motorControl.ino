fee20cf027b18c6621662848942a1b0e974393d5
Quinn Tucker <qxzcraft@gmail.com>
2022-10-17 11:19:00 -0400

    Initial code to send sensor values from Arduino

--- 2021-2022/fasterPacman/arduino/motorControl/motorControl.ino
+++ 2021-2022/fasterPacman/arduino/motorControl/motorControl.ino
@@ -1,13 +1,20 @@
 // motor libraries
-#include <Wire.h>
 #include <Adafruit_MotorShield.h>
+#include <Encoder.h>
+#include <Wire.h>
+
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 // motor setup
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *myMotor1 = AFMS.getMotor(1);
 Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
+// Encoder setup
+Encoder encoder1(2, 1);
+Encoder encoder2(3, 4);
 
+// Sensor averaging value
+float movingAverageA1;
 
 void setup() {
   // put your setup code here, to run once:
@@ -18,16 +25,39 @@ void setup() {
   // flashing light setup
   pinMode(13, OUTPUT);
 
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  movingAverageA1 = sensorValue * (5.0 / 1023.0);
+
   // serial setup
   Serial.begin(9600);
 }
 
 void loop() {
+  // read in the 2 encoders
+  long readEncoder1 = encoder1.read();
+  long readEncoder2 = encoder2.read();
+
+  // print out the encoder values
+  Serial.print("encoder1 = ");
+  Serial.print(readEncoder1);
+  Serial.print(", encoder2 = ");
+  Serial.print(readEncoder2);
 
   // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   float voltage = sensorValue * (5.0 / 1023.0);
+
+  // update the moving average
+  // defaulting to a = .5
+  movingAverageA1 = (1 - .5) * movingAverageA1 + .5 * voltage;
+
+  // print out the moving average
+  Serial.print(", sensor voltage = ");
+  Serial.print(voltage);
+  Serial.print("\n");
 
   if (Serial.available() > 0) {
     String data = Serial.readStringUntil('\n');
@@ -41,19 +71,12 @@ void loop() {
     int motorNumber = motorNumberStr.toInt();
     int motorSpeed = motorSpeedStr.toInt();
 
-    // read the input on analog pin 0:
-    int sensorValue = analogRead(A0);
-    // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-    float voltage = sensorValue * (5.0 / 1023.0);
-
     Serial.print("   you want to turn motor: ");
     Serial.print(motorNumber);
     Serial.print(" at speed ");
     Serial.print(motorSpeed);
-    Serial.print("; voltage = ");
-    Serial.print(voltage);
     Serial.print("\n");
-    
+
     if (motorSpeed == 0) {
       digitalWrite(13, LOW);
       if (motorNumber == 1) {
@@ -79,6 +102,5 @@ void loop() {
         }
       }
     }
-    
   }
 }
