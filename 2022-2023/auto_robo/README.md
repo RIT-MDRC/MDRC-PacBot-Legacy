5a9378bc4b812a06c8da44081d7cd317e7c17473
MJE10 <63426573+MJE10@users.noreply.github.com>
2023-04-07 12:12:41 -0400

    AutoRobo server can now run each component

--- 2022-2023/auto_robo/README.md
+++ 2022-2023/auto_robo/README.md
@@ -10,3 +10,11 @@ Facilitates running the Pacbot server and client code provided by HarvardURC.
 
 - You must have Python 3 installed
 - In order to properly set up the git submodules in this folder, you must run `git submodule update --init --recursive` from the root of the repository
+
+If pygame complains about no display device:
+
+- Install and run VcXsrv on windows
+  - Multiple displays
+  - Start no client
+  - Default display 0
+- Set the DISPLAY environment variable to `<<your local IP>>:0`
--- 2022-2023/auto_robo/auto_robo.py
+++ 2022-2023/auto_robo/auto_robo.py
@@ -2,6 +2,9 @@
 # @date 2023-04-06
 
 import os
+import subprocess
+import sys
+import threading
 import robomodules as rm
 import argparse as ap
 
@@ -17,8 +20,110 @@ class AutoRobo:
     It uses robomodules, a custom library for communicating with the Harvard Robotics Club's server.
     """
 
-    def __init__(self):
-        pass
+    ADDRESS = 'localhost'
+    PORT = 11297
+
+    GAME_ENGINE_PATH = os.path.join('Pacbot', 'src', 'gameEngine')
+    LOGS_PATH = None
+
+    processes = {
+        'server': {
+            'file': 'server.py',
+            'running': False,
+            'process': None
+        },
+        'gameEngine': {
+            'file': 'gameEngine.py',
+            'running': False,
+            'process': None
+        },
+        'visualize': {
+            'file': 'visualize.py',
+            'running': False,
+            'process': None
+        },
+        'terminalPrinter': {
+            'file': 'terminalPrinter.py',
+            'running': False,
+            'process': None
+        },
+        'keyboardInput': {
+            'file': 'keyboardInput.py',
+            'running': False,
+            'process': None
+        }
+    }
+
+    def __init__(self, addr, port, game_engine_path=None, logs_path=None):
+        """
+        Builds a new AutoRobo object.
+        @param addr: The address of the server
+        @param port: The port of the server
+        """
+        self.ADDRESS = addr
+        self.PORT = port
+
+        if game_engine_path is not None:
+            self.GAME_ENGINE_PATH = game_engine_path
+
+        if logs_path is not None:
+            self.LOGS_PATH = logs_path
+
+        self.start_process('server')
+
+    def start_process(self, process_name):
+        """
+        Starts a process.
+        """
+        if self.processes[process_name]['running']:
+            return
+
+        env = os.environ.copy()
+        env.update({'BIND_ADDRESS': self.ADDRESS, 'BIND_PORT': str(self.PORT)})
+
+        self.processes[process_name]['running'] = True
+        if self.LOGS_PATH is not None:
+            with open(os.path.join(self.LOGS_PATH, f'{process_name}_stdout.log'), 'w') as f:
+                with open(os.path.join(self.LOGS_PATH, f'{process_name}_stderr.log'), 'w') as g:
+                    self.processes[process_name]['process'] = subprocess.Popen(
+                        [sys.executable, os.path.join(self.GAME_ENGINE_PATH, self.processes[process_name]['file'])],
+                        stdin=subprocess.PIPE,
+                        stdout=f,
+                        stderr=g,
+                        text=True,
+                        env=env)
+        else:
+            self.processes[process_name]['process'] = subprocess.Popen(
+                [sys.executable, self.processes[process_name]['file']],
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+                env={'BIND_ADDRESS': self.ADDRESS, 'BIND_PORT': str(self.PORT)})
+
+        print(f'{process_name} started')
+
+    def stop_process(self, process_name):
+        """
+        Stops a process.
+        """
+        if not self.processes[process_name]['running']:
+            return
+
+        # don't allow other processes to run without the server
+        if process_name == 'server':
+            for proc in self.processes:
+                if proc != 'server':
+                    self.stop_process(proc)
+
+        self.processes[process_name]['process'].terminate()
+        self.processes[process_name]['process'] = None
+        self.processes[process_name]['running'] = False
+
+        print(f'{process_name} stopped')
+
+        if process_name == 'server':
+            exit()
 
 
 class AutoRoboClient(rm.ProtoModule):
@@ -90,7 +195,42 @@ if __name__ == "__main__":
     parser.add_argument('--address', '-a', type=str, default='localhost', help='The address of the server')
     parser.add_argument('--port', '-p', type=int, default=11297, help='The port of the server')
 
+    parser.add_argument('--game-engine-path', '-g', type=str, default=None, help='The path to the game engine folder')
+    parser.add_argument('--logs-path', '-l', type=str, default=None, help='The path to the logs folder')
+
     args = parser.parse_args()
 
-    # Create the client
-    client = AutoRoboClient(args.address, args.port)
+    # Create the server
+    server = AutoRobo(args.address, args.port, args.game_engine_path, args.logs_path)
+
+    # User menu
+    while True:
+        print('''
+0) Stop Server
+1) Start Game Engine
+2) Stop Game Engine
+3) Start Visualization
+4) Stop Visualization
+5) Start Terminal Printer
+6) Start Keyboard Input
+''')
+
+        choice = input('Enter your choice: ')
+
+        match choice:
+            case '0':
+                server.stop_process('server')
+            case '1':
+                server.start_process('gameEngine')
+            case '2':
+                server.stop_process('gameEngine')
+            case '3':
+                server.start_process('visualize')
+            case '4':
+                server.stop_process('visualize')
+            case '5':
+                server.start_process('terminalPrinter')
+            case '6':
+                server.start_process('keyboardInput')
+            case _:
+                print('Invalid choice')
