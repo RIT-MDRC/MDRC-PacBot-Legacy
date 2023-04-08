63bd37796d2df1ccd95a4dd7e724c371fb0a31d6
Christian Secular <cds5872@rit.edu>
2023-04-08 15:37:19 -0400

    Low level code: encoder feedback and reading IR sensor data. TO BE TESTED

--- 2021-2022/fasterPacman/arduino/motorControl/motorControl.ino
+++ 2021-2022/fasterPacman/arduino/motorControl/motorControl.ino
@@ -14,51 +14,78 @@ Encoder encoder1(2, 1);
 Encoder encoder2(3, 4);
 
 // Sensor averaging value
-float movingAverageA1;
+float movingAverages[5];
 
+//Motor1 = Left, Motor2 = right
+int motor1_targetVel; //target velocity for feedback control
+int motor2_targetVel;
+
+int motor1_inputVel; //value to send to motors
+int motor2_inputVel;
+//counters for amount of rising edges on pins 2 and 3 from encoders.
+volatile unsigned long enc1_count = 0;
+volatile unsigned long enc2_count = 0;
 void setup() {
   // put your setup code here, to run once:
   // motor setup
   AFMS.begin();
-  myMotor1->setSpeed(255);
 
   // flashing light setup
   pinMode(13, OUTPUT);
 
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  movingAverageA1 = sensorValue * (5.0 / 1023.0);
-
   // serial setup
   Serial.begin(9600);
+
+  pinMode(2,INPUT_PULLUP);
+  attachInterrupt(0, isr_enc1_count, RISING); //interrupt signal to pin 2
+
+  pinMode(3,INPUT_PULLUP);
+  attachInterrupt(0, isr_enc2_count, RISING); //interrupt signal to pin 2
+
+  //read IR sensors initial value
+  read_ir_init();
 }
 
 void loop() {
-  // read in the 2 encoders
-  long readEncoder1 = encoder1.read();
-  long readEncoder2 = encoder2.read();
+  
+  /* UNCOMMENT TO TEST VELOCITY CONTROL (NOT WHEELS SEPARATELY), REQUIRES ENCODERS*/
+  
+  //read ir and adjust weigthed averages
+  read_ir_update();
 
-  // print out the encoder values
-  Serial.print("encoder1 = ");
-  Serial.print(readEncoder1);
-  Serial.print(", encoder2 = ");
-  Serial.print(readEncoder2);
+  //run velocity control on motors
+  velocity_control();
 
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
+  myMotor1->setSpeed(abs(motor1_inputVel));
+  if(motor1_targetVel == 0)
+  {
+    myMotor1->run(RELEASE);
+  } else if (motor1_targetVel < 0) {
+    myMotor1->run(BACKWARD);
+  } else {
+    myMotor1->run(FORWARD);
+  }
 
-  // update the moving average
-  // defaulting to a = .5
-  movingAverageA1 = (1 - .5) * movingAverageA1 + .5 * voltage;
+  myMotor2->setSpeed(abs(motor2_inputVel));
+  if(motor2_targetVel == 0)
+  {
+    myMotor2->run(RELEASE);
+  } else if (motor1_targetVel < 0) {
+    myMotor2->run(BACKWARD);
+  } else {
+    myMotor2->run(FORWARD);
+  }
+  /*----COMMENT END */
 
-  // print out the moving average
-  Serial.print(", sensor voltage = ");
-  Serial.print(voltage);
-  Serial.print("\n");
-
+  //prints of IR data
+  for(int i = 0; i < 5; i++)
+  {
+    Serial.print("Sensor: ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(movingAverages[i]);
+  }
+  //if command available in terminal
   if (Serial.available() > 0) {
     String data = Serial.readStringUntil('\n');
     Serial.print("echo: ");
@@ -76,7 +103,7 @@ void loop() {
     Serial.print(" at speed ");
     Serial.print(motorSpeed);
     Serial.print("\n");
-
+    
     if (motorSpeed == 0) {
       digitalWrite(13, LOW);
       if (motorNumber == 1) {
@@ -87,14 +114,14 @@ void loop() {
     } else {
       digitalWrite(13, HIGH);
       if (motorNumber == 1) {
-        myMotor1->setSpeed(abs(motorSpeed));
+        motor1_targetVel = motorSpeed;
         if (motorSpeed < 0) {
           myMotor1->run(BACKWARD);
         } else {
           myMotor1->run(FORWARD);
         }
       } else if (motorNumber == 2) {
-        myMotor2->setSpeed(abs(motorSpeed));
+        motor2_targetVel = motorSpeed;
         if (motorSpeed < 0) {
           myMotor2->run(BACKWARD);
         } else {
@@ -102,5 +129,98 @@ void loop() {
         }
       }
     }
+  }
+
+  delay(10);
+}
+
+//increment counter everytime an edge occurs on pin 2 (Enc1)
+void isr_enc1_count()
+{
+  enc1_count++;
+}
+
+
+//increment counter everytime an edge occurs on pin 3 (Enc2)
+void isr_enc2_count()
+{
+  enc2_count++;
+}
+
+void read_ir_init()
+{
+  int sensorValue;
+  static const uint8_t analog_pins[] = {A0,A1,A2,A3,A4};
+  for(int i = 0; i < 5; i++)
+  {
+    sensorValue = analogRead(analog_pins[i]);
+    movingAverages[i] =(float) sensorValue * (5.0 / 1023.0);
+  }
+}
+
+void read_ir_update()
+{
+  int sensorValue;
+  float new_voltage;
+  static const uint8_t analog_pins[] = {A0,A1,A2,A3,A4};
+  for(int i = 0; i < 5; i++)
+  {
+    sensorValue = analogRead(analog_pins[i]);
+    new_voltage = sensorValue * (5.0 / 1023.0);
+    // update the moving average
+    // defaulting to a = .5
+    movingAverages[i] = (1 - .5) * movingAverages[i] + .5 * new_voltage;
+  }
+}
+
+//run both wheels forward
+void straight(int vel)
+{
+  motor1_targetVel = vel;
+  motor2_targetVel = vel;
+}
+
+//run left wheel backwards and right wheel forward
+void turn_left(int vel)
+{
+  motor1_targetVel = -vel;
+  motor2_targetVel = vel;
+}
+
+//run right wheel backwards and left wheel forward
+void turn_right(int vel)
+{
+  motor1_targetVel = vel;
+  motor2_targetVel = -vel;
+}
+
+void velocity_control()
+{
+  unsigned int motor1_ticks = enc1_count;
+  unsigned int motor2_ticks = enc2_count;
+
+  enc1_count = 0;
+  enc2_count = 0;
+  unsigned int rpm_convert = 1;
+  float motor1_calcVel = (float) motor1_ticks / rpm_convert //10ms ideally passes
+  float motor2_calcVel = (float) motor2_ticks / rpm_convert //10ms ideally passes, will need to  tune
+  //Motor 1
+  //if greater than target velocity, decrease input to the motor, else if less than target velocity, increase input to the motor
+  if((motor1_calcVel > motor1_targetVel) | motor1_inputVel > 0)
+  {
+    motor1_inputVel --;
+  } else if((motor1_ticks/rpm_convert < motor1_targetVel) | motor1_inputVel < 256)
+  {
+    motor1_inputVel ++;
+  }
+
+  //Motor 2
+  //if greater than target velocity, decrease input to the motor, else if less than target velocity, increase input to the motor
+  if((motor2_calcVel > motor2_targetVel) | motor2_inputVel > 0)
+  {
+    motor2_inputVel --;
+  } else if((motor2_ticks/rpm_convert < motor2_targetVel) | motor2_inputVel < 256)
+  {
+    motor2_inputVel ++;
   }
 }
