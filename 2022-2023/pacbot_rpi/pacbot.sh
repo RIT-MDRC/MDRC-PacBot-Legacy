8dbf4831bcf18c0a3b6a125896ed4a6b637cabec
Ben Giacalone <bensgiacalone@gmail.com>
2023-04-29 01:56:30 -0400

    Competition settings

--- 2022-2023/pacbot_rpi/pacbot.sh
+++ 2022-2023/pacbot_rpi/pacbot.sh
@@ -1,28 +1,28 @@
 # required for visuals
 export PYTHONUNBUFFERED=1
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
-export DISPLAY=eliaspectre.student.rit.edu:0
+#export DISPLAY=eliaspectre.student.rit.edu:0
 
 # pacbot specific
-export ADDRESS=eliaspectre.student.rit.edu
+export ADDRESS=192.168.0.100
 export PORT=11297
 
 export USE_REAL_ARDUINO=t
 export PI_SERIAL_PORT=/dev/ttyUSB0
-export USE_PROJECTOR=f
-export DISABLE_MOTORS=t
-export FORCE_PF_POSITION=4,29
+export USE_PROJECTOR=t
+export DISABLE_MOTORS=f
+#export FORCE_PF_POSITION=4,29
 
 # HEURISTIC or MCTS
-export PATH_TYPE=MCTS
+export PATH_TYPE=HEURISTIC
 
 # debug, f or t
 export DEBUG_JSON=f
 # game state currently broken
 export DEBUG_GAME_STATE=f
 export DEBUG_PF_INFO=f
-export DEBUG_VIS=t
+export DEBUG_VIS=f
 
-export MOVE_WHEN_PAUSED=t
+export MOVE_WHEN_PAUSED=f
 
 python3 ~/MDRC-PacBot/2022-2023/pacbot_rpi/pacbot.py
