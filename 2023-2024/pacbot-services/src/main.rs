944c52f2909e933ae3e94f50d3d870c6f5e88b85
MJE10 <63426573+MJE10@users.noreply.github.com>
2023-06-15 21:38:08 -0400

    Add log4rs

--- 2023-2024/pacbot-services/src/main.rs
+++ 2023-2024/pacbot-services/src/main.rs
@@ -1,6 +1,12 @@
+use log::warn;
+
 mod model;
 mod service;
 
 fn main() {
     println!("Hello, world!");
+
+    log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+
+    warn!("oops!");
 }
