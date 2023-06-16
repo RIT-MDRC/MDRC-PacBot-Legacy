a97d73ecedda6209ab0218164fe20d35c3082624
MJE10 <63426573+MJE10@users.noreply.github.com>
2023-06-16 16:32:38 -0400

    Remake Vue project with Vuetify, add basic UI

--- 2023-2024/pacbot-gui/pacbot-vue/README.md
+++ 2023-2024/pacbot-gui/pacbot-vue/README.md
@@ -1,71 +1,44 @@
-# pacbot-vue
+# default
 
-This template should help get you started developing with Vue 3 in Vite.
+## Project setup
 
-## Recommended IDE Setup
+```
+# yarn
+yarn
 
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
-
-## Type Support for `.vue` Imports in TS
-
-TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin) to make the TypeScript language service aware of `.vue` types.
-
-If the standalone TypeScript plugin doesn't feel fast enough to you, Volar has also implemented a [Take Over Mode](https://github.com/johnsoncodehk/volar/discussions/471#discussioncomment-1361669) that is more performant. You can enable it by the following steps:
-
-1. Disable the built-in TypeScript Extension
-    1) Run `Extensions: Show Built-in Extensions` from VSCode's command palette
-    2) Find `TypeScript and JavaScript Language Features`, right click and select `Disable (Workspace)`
-2. Reload the VSCode window by running `Developer: Reload Window` from the command palette.
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vitejs.dev/config/).
-
-## Project Setup
-
-```sh
+# npm
 npm install
+
+# pnpm
+pnpm install
 ```
 
-### Compile and Hot-Reload for Development
+### Compiles and hot-reloads for development
 
-```sh
+```
+# yarn
+yarn dev
+
+# npm
 npm run dev
+
+# pnpm
+pnpm dev
 ```
 
-### Type-Check, Compile and Minify for Production
+### Compiles and minifies for production
 
-```sh
-npm run build
 ```
+# yarn
+yarn build
 
-### Run Unit Tests with [Vitest](https://vitest.dev/)
-
-```sh
-npm run test:unit
-```
-
-### Run End-to-End Tests with [Playwright](https://playwright.dev)
-
-```sh
-# Install browsers for the first run
-npx playwright install
-
-# When testing on CI, must build the project first
+# npm
 npm run build
 
-# Runs the end-to-end tests
-npm run test:e2e
-# Runs the tests only on Chromium
-npm run test:e2e -- --project=chromium
-# Runs the tests of a specific file
-npm run test:e2e -- tests/example.spec.ts
-# Runs the tests in debug mode
-npm run test:e2e -- --debug
+# pnpm
+pnpm build
 ```
 
-### Lint with [ESLint](https://eslint.org/)
+### Customize configuration
 
-```sh
-npm run lint
-```
+See [Configuration Reference](https://vitejs.dev/config/).
--- 2023-2024/pacbot-gui/pacbot-vue/src/components/HelloWorld.vue
+++ 2023-2024/pacbot-gui/pacbot-vue/src/components/HelloWorld.vue
@@ -1,40 +1,120 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
-  </div>
+  <v-toolbar>
+    <v-toolbar-title>RIT Pacbot</v-toolbar-title>
+  </v-toolbar>
+  <v-row class="fill-height">
+    <v-col>
+      <v-container class="text-center">
+        
+      </v-container>
+    </v-col>
+    <v-col>
+      <div class="d-flex flex-row">
+        <v-tabs
+          v-model="tab"
+          direction="vertical"
+          color="primary"
+        >
+          <v-tab v-for="service in services" :value="service.name">
+            <v-icon v-if="service.icon" start>
+              mdi-account
+            </v-icon>
+            {{ service.name }}
+          </v-tab>
+        </v-tabs>
+        <v-window v-model="tab" style="flex-grow: 1;" class="ml-2">
+          <v-window-item v-for="service in services" :value="service.name">
+            <div v-for="status in service.statuses" class="d-flex align-center mt-2">
+              <v-chip :color="status.status" class="mr-2">
+                {{ status.chip }}
+                <v-tooltip
+                  v-if="status.message"
+                  activator="parent"
+                  location="bottom"
+                >{{ status.message }}</v-tooltip>
+              </v-chip>
+              <div>{{ status.name }}</div>
+            </div>
+            <v-divider class="mt-2"></v-divider>
+            <v-checkbox label="Checkbox" hide-details="auto"></v-checkbox>
+            <v-combobox
+              hide-details="auto"
+              label="Combobox"
+              :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+            ></v-combobox>
+            <v-divider class="mt-2"></v-divider>
+            <div v-for="debug in service.debug" class="d-flex align-center mt-2">
+              {{ debug.name }}: {{ debug.value }}
+            </div>
+          </v-window-item>
+        </v-window>
+      </div>
+    </v-col>
+  </v-row>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
+<style>
+  /* h2, h3 {
+    margin-bottom: 10px;
   }
-}
+
+  h3 {
+    margin-top: 10px;
+  } */
 </style>
+
+<script setup lang="ts">
+  import { stat } from 'fs';
+import { ref } from 'vue'
+
+  let tab = ref('option-1');
+
+  let services = [
+    {
+      id: 0,
+      name: 'Robomodules Client',
+      status: 'success',
+      chip: 'OK',
+      statuses: [
+        {
+          name: 'Internet Connection',
+          status: 'success',
+          chip: 'OK'
+        },
+        {
+          name: 'Robomodules Connection',
+          status: 'warning',
+          chip: 'WARNING',
+          message: 'Connecting...'
+        }
+      ],
+      options: [
+        {
+          name: 'Active',
+          type: 'checkbox'
+        }
+      ],
+      debug: [
+        {
+          name: 'Test',
+          value: 123
+        }
+      ]
+    },
+    {
+      id: 1,
+      name: 'Particle Filter',
+      status: 'error',
+      chip: 'ERROR',
+      icon: 'mdi-information-outline',
+      statuses: [],
+      options: [
+        {
+          name: 'Number of Particles',
+          type: 'number'
+        }
+      ],
+      debug: []
+    }
+  ]
+</script>
