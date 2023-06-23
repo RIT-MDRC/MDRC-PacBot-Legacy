1e62b80685535148d74a7a679fc9242398b12c03
MJE10 <63426573+MJE10@users.noreply.github.com>
2023-06-23 16:05:02 -0400

    Allow dragging of sub-windows

--- 2023-2024/pacbot-gui/pacbot-svelte/src/routes/recursivePage.svelte
+++ 2023-2024/pacbot-gui/pacbot-svelte/src/routes/recursivePage.svelte
@@ -2,13 +2,54 @@
 	import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
 	import IconButton from '@smui/icon-button';
 	import RecursivePage from './recursivePage.svelte';
+	import { onMount } from 'svelte';
 
 	export let is_top_level: boolean = false;
 
 	let content_type: '' | 'horizontal' | 'vertical' | 'settings' = '';
+
+	let vertical_split_width = 0;
+	let horizontal_split_width = 0;
+
+	let content: any;
+
+	onMount(() => {
+		vertical_split_width = content.offsetWidth / 2;
+		horizontal_split_width = content.offsetHeight / 2;
+	});
+
+	let vertical_move_start = -1;
+	let vertical_move_client_start = -1;
+
+	let horizontal_move_start = -1;
+	let horizontal_move_client_start = -1;
+
+	function onVerticalPointerDown(e: any) {
+		vertical_move_start = vertical_split_width;
+		vertical_move_client_start = e.clientX;
+	}
+
+	function onHorizontalPointerDown(e: any) {
+		horizontal_move_start = horizontal_split_width;
+		horizontal_move_client_start = e.clientY;
+	}
+
+	function onPointerMove(e: any) {
+		if (horizontal_move_start != -1) {
+			horizontal_split_width = horizontal_move_start - (horizontal_move_client_start - e.clientY);
+		}
+		if (vertical_move_start != -1) {
+			vertical_split_width = vertical_move_start - (vertical_move_client_start - e.clientX);
+		}
+	}
+
+	function onPointerUp(e: any) {
+		vertical_move_start = -1;
+		horizontal_move_start = -1;
+	}
 </script>
 
-<div id="container">
+<div id="container" on:pointermove={onPointerMove} on:pointerup={onPointerUp}>
 	<div>
 		<TopAppBar variant="static" color="primary" dense>
 			<Row>
@@ -45,27 +86,30 @@
 		</TopAppBar>
 	</div>
 
-	<div id="content">
+	<div id="content" bind:this={content}>
 		{#if content_type == 'vertical'}
 			<div id="content-vertical">
-				<div id="content-left">
+				<div id="content-left" style="width: {vertical_split_width}px">
 					<RecursivePage />
 				</div>
+				<div id="vertical-split-resizer" on:pointerdown={onVerticalPointerDown} />
 				<div id="content-right">
 					<RecursivePage />
 				</div>
 			</div>
 		{:else if content_type == 'horizontal'}
 			<div id="content-horizontal">
-				<div id="content-top">
+				<div id="content-top" style="height: {horizontal_split_width}px">
 					<RecursivePage />
 				</div>
+				<div id="horizontal-split-resizer" on:pointerdown={onHorizontalPointerDown} />
 				<div id="content-bottom">
 					<RecursivePage />
 				</div>
 			</div>
 		{:else if content_type == 'settings'}
 			<p>settings</p>
+			<IconButton class="material-icons" on:click={() => (content_type = '')}>close</IconButton>
 		{:else if content_type == ''}
 			<p>hello</p>
 		{/if}
@@ -93,12 +137,8 @@
 		height: 100%;
 	}
 
-	#content-left {
-		width: 50%;
-	}
-
 	#vertical-split-resizer {
-		width: 3px;
+		width: 5px;
 		background-color: gray;
 		border: 1px solid black;
 		cursor: grab;
@@ -115,8 +155,11 @@
 		height: 100%;
 	}
 
-	#content-top {
-		height: 50%;
+	#horizontal-split-resizer {
+		height: 5px;
+		background-color: gray;
+		border: 1px solid black;
+		cursor: grab;
 	}
 
 	#content-bottom {
