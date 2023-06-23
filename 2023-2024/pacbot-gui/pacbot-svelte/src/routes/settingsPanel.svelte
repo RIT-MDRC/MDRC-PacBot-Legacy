9f0ead05c9602a3bd191d3087f7d3e6e2c5c8640
MJE10 <63426573+MJE10@users.noreply.github.com>
2023-06-23 16:44:56 -0400

    improve settings panel

---
+++ 2023-2024/pacbot-gui/pacbot-svelte/src/routes/settingsPanel.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Accordion, { Panel, Header, Content } from '@smui-extra/accordion';
+</script>
+
+<div class="accordion-container">
+	<Accordion>
+		<Panel>
+			<Header>Panel 1</Header>
+			<Content />
+		</Panel>
+		<Panel>
+			<Header>Panel 2</Header>
+			<Content>The content for panel 2.</Content>
+		</Panel>
+		<Panel>
+			<Header>Panel 3</Header>
+			<Content />
+		</Panel>
+		<Panel>
+			<Header>Panel 4</Header>
+			<Content />
+		</Panel>
+	</Accordion>
+</div>
